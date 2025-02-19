88b2111a6003030c895ca3aa7e7b2c97d8e3cdc3
SandyOliveira <sandyoliveira828@gmail.com>
2025-02-19 15:39:20 -0300

    feat: Implemwnting connection in DB and adding repository

---
+++ src/model/repositories/eventos_repository_test.py
@@ -0,0 +1,21 @@
+import pytest
+from .eventos_repository import EventosRepository
+
+@pytest.mark.skip("Insert in DB")
+#adicionar evento no DB
+def test_insert_eventos():
+    event_name = "event2"
+    event_repo = EventosRepository()
+
+    
+    event_repo.insert(event_name)
+    
+@pytest.mark.skip("Select in DB") 
+#selicionar Evento 
+def test_select_event():
+    event_name = "event2"
+    event_repo = EventosRepository()
+    
+    event = event_repo.select_event(event_name)
+    # print(event)
+    # print(event.nome)
