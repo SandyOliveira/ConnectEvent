46b0325411407c0d4ce5f235a18b50a46120dfee
SandyOliveira <sandyoliveira828@gmail.com>
2025-02-20 00:02:54 -0300

    feat: Implementing validators

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -2,6 +2,8 @@ from flask import Blueprint,jsonify, request
 
 event_route_bp =Blueprint("event_route",__name__)
 
+from src.validators.events_creator_validator import events_creator_validator
+
 
 from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest
@@ -10,8 +12,8 @@ from src.http_types.http_request import HttpRequest
 @event_route_bp.route("/event",methods=["POST"])
 
 def create_new_event():
+    events_creator_validator(request)
     http_request = HttpRequest(body=request.json)
-    print(http_request.body)
     http_response = HttpResponse(body={"local":"aqui"},status_code =201)
     return jsonify(http_response.body),http_response.status_code
 
