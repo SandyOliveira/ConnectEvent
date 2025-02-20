f2d9ba921857aa2325b03665bf5075e6798b6243
SandyOliveira <sandyoliveira828@gmail.com>
2025-02-20 01:50:57 -0300

    feat: Implementing events_link

---
+++ src/model/repositories/eventos_link_repository_test.py
@@ -0,0 +1,9 @@
+import pytest
+from .eventos_link_repository import EventosLinkRepository
+@pytest.mark.skip("Insert in DB")
+def test_insert_eventos_link():
+    event_id = 16
+    subs_id = 14
+    event_link_repo = EventosLinkRepository()
+    
+    event_link_repo.insert(event_id,subs_id)
